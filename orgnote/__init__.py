adf1f87c7b8746ca521c84f2d784eaf9394c7a78
Leslie Zhu <pythonisland@gmail.com>
2014-11-24 16:27:53 +0800

    build wheel file and use pip to install it under virtualenv

---
+++ orgnote/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+__version__ = "0.3.0"
+
+def main():
+    import orgnote.__main__
+    orgnote.__main__.main()
---
+++ orgnote/init.py
@@ -0,0 +1,27 @@
+#-*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+
+def main(args=None):
+    import os
+    import os.path
+    
+    cur_dir = os.popen("pwd").readline().strip()
+    
+    path_sep = os.path.sep
+    orgnote_dir = os.path.abspath(os.path.dirname(__file__))
+    OrgNote_dir = path_sep.join(orgnote_dir.split(path_sep)[:-1])
+    
+    target_list = ["theme","notes","init-orgnote.el","public"]
+
+    for target in target_list:
+        from_path = OrgNote_dir + path_sep + target
+        print "init dir/file ",target
+        os.system("cp -r %s %s" % (from_path,cur_dir))
+    
+
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
