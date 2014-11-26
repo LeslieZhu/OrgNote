84b39804e4b9b21b062d4b818da074e2172ce65e
Leslie Zhu <pythonisland@gmail.com>
2014-11-26 15:53:11 +0800

    use config.py, not Confog.py

---
+++ orgnote/_config.py
@@ -0,0 +1,11 @@
+#-*- coding: utf-8 -*-
+
+"""
+This is the config file for OrgNote.
+"""
+
+__title__ = "OrgNote"
+__subtitle__ = "OrgNote"
+__author__ = "OrgNote"
+__description__ = "My information"
+__blog_keywords__ = "My Blog keywords"
