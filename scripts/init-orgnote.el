3f463b67498702660ae232b5b18c7a0bc48d0dd8
Leslie Zhu <pythonisland@gmail.com>
2014-11-26 15:40:35 +0800

    scripts/ replace with git@github.com:LeslieZhu/orgnote-emacs-el.git

--- scripts/init-orgnote.el
+++
@@ -1,9 +0,0 @@
-;; 设置org-mode输出中文目录
-(custom-set-variables
- '(org-blank-before-new-entry 
-   (quote ((heading) (plain-list-item))))
-
- '(org-export-language-setup 
-   (quote 
-    (("en" "Author" "Date" "Table of Contents" "Footnotes") ("zh-CN" "作者" "日期" "目录" "脚注")))))
- 
