7f4dd78e25a4cf9766058f7ab3ee8665fc5ff79b
Leslie Zhu <pythonisland@gmail.com>
2014-12-02 16:51:46 +0800

    update common comments

--- orgnote/__init__.py
+++ orgnote/__init__.py
@@ -1,3 +1,13 @@
+"""
+OrgNote  ---- A simple org-mode blog, write blog by org-mode in Emacs
+
+author: Leslie Zhu
+email: pythonisland@gmail.com
+
+Write note by Emacs with org-mode, and convert .org file into .html file,
+then use orgnote convert into new html with default theme.
+"""
+
 from __future__ import absolute_import
 
 __version__ = "0.3.0"
--- orgnote/config.py
+++ orgnote/config.py
@@ -1,5 +1,15 @@
 #-*- coding: utf-8 -*-
 
+"""
+OrgNote  ---- A simple org-mode blog, write blog by org-mode in Emacs
+
+author: Leslie Zhu
+email: pythonisland@gmail.com
+
+Write note by Emacs with org-mode, and convert .org file into .html file,
+then use orgnote convert into new html with default theme.
+"""
+
 from __future__ import absolute_import
 
 import ConfigParser
