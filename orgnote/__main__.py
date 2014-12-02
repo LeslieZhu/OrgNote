af3eda75d1dcad3635165315d87095eca6f0d8e5
Leslie Zhu <pythonisland@gmail.com>
2014-12-02 16:50:25 +0800

    update common comments

--- orgnote/__main__.py
+++ orgnote/__main__.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-# OrgNote  ---- A simple org-mode blog, powered by python
-# 
-# orgnote.py ---- write blog by org-mode note
-# 
-# author: Leslie Zhu
-# email: pythonisland@gmail.com
-#
-# Write note by Emacs with org-mode, and convert .org file itno .html file
-# thus use orgnote.py convert into new html with speciall css
-# 
+"""
+OrgNote  ---- A simple org-mode blog, write blog by org-mode in Emacs
+
+author: Leslie Zhu
+email: pythonisland@gmail.com
+
+Write note by Emacs with org-mode, and convert .org file into .html file,
+then use orgnote convert into new html with default theme.
+"""
 
 from __future__ import absolute_import
 
