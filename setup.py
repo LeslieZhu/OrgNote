63a1b2b86e7f67022ddca8375aa2de7a5b1d3eb8
Leslie Zhu <pythonisland@gmail.com>
2014-12-08 01:59:44 +0800

    add DESCRIPTION.rst

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
-long_description = read('README.rst')
+long_description = read('DESCRIPTION.rst')
 
 setup(
     name = "orgnote",
