84798070bad0e5b8d5491afce71fb19923e0bac7
Leslie Zhu <pythonisland@gmail.com>
2016-07-21 18:35:23 +0800

    it's still 0.4.9 version

--- orgnote/__init__.py
+++ orgnote/__init__.py
@@ -10,7 +10,7 @@ then use orgnote convert into new html with default theme.
 
 from __future__ import absolute_import
 
-__version__ = "0.5.0"
+__version__ = "0.4.9"
 
 def main():
     import orgnote.__main__
