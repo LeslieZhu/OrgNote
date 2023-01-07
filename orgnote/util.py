d340bda72b519db27b8f80e10b0a3a88ca9ac00d
Leslie Zhu <pythonisland@gmail.com>
2023-01-07 21:40:54 +0800

    default lang is 'text'

--- orgnote/util.py
+++ orgnote/util.py
@@ -22,7 +22,9 @@ from pygments.formatters import html as py_html
  
 class HighlightRenderer(mistune.HTMLRenderer):
  
-    def block_code(self, code, lang):
+    def block_code(self, code, lang="text"):
+        if lang.strip() == "":
+            lang = "text"
         if not lang:
             return '\n<pre><code>%s</code></pre>\n' % mistune.escape(code)
         lexer = get_lexer_by_name(lang, stripall=True)
